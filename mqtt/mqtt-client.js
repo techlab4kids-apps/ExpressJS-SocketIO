5be1195406d78d6a1f493043e6f823e40889050f
giancarlo.orru <techlab4kids.apps@gmail.com>
2023-01-01 18:01:03 +0100

    The server act now as a mqtt proxy to and from Socket IO messages

--- mqtt/mqtt-client.js
+++ mqtt/mqtt-client.js
@@ -1,77 +1,77 @@
 const mqtt = require('mqtt');
 
 
-// const flespiConfig = {
-//     host: new URL("https://mqtt.flespi.io"),
-//     port: 1883,
-//     username: "sVOnzW676JCdey42x2AcIMKdn5XUeeJu0lIn8Cj29J4e7Y4uOuuk42ohXb20fzk3",
-//     password: "",
-//     clientId: "TL4K-SocketIO-Mqtt-Proxy"
-// }
-//
-// const localConfig = {
-//     host: new URL("http://localhost"),
-//     port: 1883,
-//     username: "",
-//     password: "",
-//     clientId: "TL4K-SocketIO-Mqtt-Proxy"
-// }
-//
-// /*
-//     Agrumino data mqtt payload
-//     String payload = "{";
-//     payload += "\"temperatura\":";
-//     payload += agruminoData.temperature;
-//     payload += ",";
-//     payload += "\"umidita' del terreno\":";
-//     payload += agruminoData.soilMoisture;
-//     payload += ",";
-//     payload += "\"luminosita'\":";
-//     payload += agruminoData.illuminance;
-//     payload += ",";
-//     payload += "\"stato del led\":";
-//     payload += agruminoData.isLedOn;
-//     // payload += ",";
-//
-//     // payload += "\"batteryVoltage\":";
-//     // payload += agruminoData.batteryVoltage;
-//     // payload += ",";
-//     // payload += "\"batteryLevel\":";
-//     // payload += agruminoData.batteryLevel;
-//     // payload += ",";
-//     // payload += "\"isAttachedToUSB\":";
-//     // payload += agruminoData.isAttachedToUSB;
-//     // payload += ",";
-//     // payload += "\"isBatteryCharging\":";
-//     // payload += agruminoData.isBatteryCharging;
-//     // payload += ",";
-//
-//     payload += "}";
-//
-//  */
-//
-// // let client = mqtt.connect("mqtt://test.mosquitto.org")
-// let client = mqtt.connect(flespiConfig)
-//
-// const topics = ["tl4k/devices/+/data", "tl4k/devices/+/command"]
-//
-// client.on('connect', () => {
-//     console.log("Connected to Flespi broker with client!")
-//     topics.forEach((topic) => {
-//         console.log("Subscribing to topic: ", topic)
-//         client.subscribe(topic)
-//     })
-// })
-//
-// client.on('error', (error) => {
-//     console.log("[Mqtt client] Error: " + JSON.stringify(error));
-// })
-//
-// client['stream'].on('error', (error) => {
-//     // fires when connection can't be established
-//     // but not when an established connection is lost
-//     console.log('[Mqtt client] stream error', error);
-//     client.end();
-// });
+const flespiConfig = {
+    username: "FlespiToken sVOnzW676JCdey42x2AcIMKdn5XUeeJu0lIn8Cj29J4e7Y4uOuuk42ohXb20fzk3",
+    password: "",
+    clientId: "TL4K-SocketIO"
+}
+
+const localConfig = {
+    host: new URL("http://localhost"),
+    port: 1883,
+    username: "",
+    password: "",
+    clientId: "TL4K-SocketIO-Mqtt-Proxy"
+}
+
+/*
+    Agrumino data mqtt payload
+    String payload = "{";
+    payload += "\"temperatura\":";
+    payload += agruminoData.temperature;
+    payload += ",";
+    payload += "\"umidita' del terreno\":";
+    payload += agruminoData.soilMoisture;
+    payload += ",";
+    payload += "\"luminosita'\":";
+    payload += agruminoData.illuminance;
+    payload += ",";
+    payload += "\"stato del led\":";
+    payload += agruminoData.isLedOn;
+    // payload += ",";
+
+    // payload += "\"batteryVoltage\":";
+    // payload += agruminoData.batteryVoltage;
+    // payload += ",";
+    // payload += "\"batteryLevel\":";
+    // payload += agruminoData.batteryLevel;
+    // payload += ",";
+    // payload += "\"isAttachedToUSB\":";
+    // payload += agruminoData.isAttachedToUSB;
+    // payload += ",";
+    // payload += "\"isBatteryCharging\":";
+    // payload += agruminoData.isBatteryCharging;
+    // payload += ",";
+
+    payload += "}";
+
+ */
+
+// let client = mqtt.connect("mqtt://test.mosquitto.org")
+let client = mqtt.connect("wss://mqtt.flespi.io", flespiConfig)
+
+const topics = ["tl4k/devices/+/data", "tl4k/devices/+/command", "tl4k/sensornode"]
+
+client.on('connect', () => {
+    console.log("Connected to Flespi broker with client!")
+    topics.forEach((topic) => {
+        console.log("Subscribing to topic: ", topic)
+        client.subscribe(topic)
+    })
+
+    // client.subscribe("tl4k/devices/#")
+})
+
+client.on('error', (error) => {
+    console.log("[Mqtt client] Error: " + JSON.stringify(error));
+})
+
+client['stream'].on('error', (error) => {
+    // fires when connection can't be established
+    // but not when an established connection is lost
+    console.log('[Mqtt client] stream error', error);
+    client.end();
+});
 
 module.exports = { client };
--- server.js
+++ server.js
@@ -99,8 +99,8 @@ mqttHttpServer.listen(mqttHttpServerPort, function () {
     console.log('mqtt websocket server listening on port ', mqttHttpServerPort)
 })
 
-//const { client } = require('./mqtt/mqtt-client');
-const mqtt = require('mqtt');
+const { client } = require('./mqtt/mqtt-client');
+// const mqtt = require('mqtt');
 
 /**
  * Socket.io section
@@ -111,6 +111,16 @@ const io = require('socket.io')(httpServer, {
         methods: ['GET', 'POST'],
     },
 });
+
+async function handleIoSocketEvent(eventName, currentSocketId, ...args) {
+    const sockets = await io.fetchSockets()
+    sockets.forEach(socket => {
+        if(socket.id != currentSocketId){
+            socket.emit(eventName, ...args);
+        }
+    });
+}
+
 io.on('connection', function(socket) {
 
     console.log(`New connection: ${socket.id}`);
@@ -119,91 +129,35 @@ io.on('connection', function(socket) {
 
     socket.on('disconnect', () => console.log(`Connection left (${socket.id})`));
 
-});
+    socket.onAny((eventName, ...args) => {
+        handleIoSocketEvent(eventName, socket.id, ...args);
+    });
 
-
-const flespiConfig = {
-    username: "FlespiToken sVOnzW676JCdey42x2AcIMKdn5XUeeJu0lIn8Cj29J4e7Y4uOuuk42ohXb20fzk3",
-    password: "",
-    clientId: "TL4K-SocketIO"
-}
-
-const localConfig = {
-    host: new URL("http://localhost"),
-    port: 1883,
-    username: "",
-    password: "",
-    clientId: "TL4K-SocketIO-Mqtt-Proxy"
-}
-
-/*
-    Agrumino data mqtt payload
-    String payload = "{";
-    payload += "\"temperatura\":";
-    payload += agruminoData.temperature;
-    payload += ",";
-    payload += "\"umidita' del terreno\":";
-    payload += agruminoData.soilMoisture;
-    payload += ",";
-    payload += "\"luminosita'\":";
-    payload += agruminoData.illuminance;
-    payload += ",";
-    payload += "\"stato del led\":";
-    payload += agruminoData.isLedOn;
-    // payload += ",";
-
-    // payload += "\"batteryVoltage\":";
-    // payload += agruminoData.batteryVoltage;
-    // payload += ",";
-    // payload += "\"batteryLevel\":";
-    // payload += agruminoData.batteryLevel;
-    // payload += ",";
-    // payload += "\"isAttachedToUSB\":";
-    // payload += agruminoData.isAttachedToUSB;
-    // payload += ",";
-    // payload += "\"isBatteryCharging\":";
-    // payload += agruminoData.isBatteryCharging;
-    // payload += ",";
-
-    payload += "}";
-
- */
-
-// let client = mqtt.connect("mqtt://test.mosquitto.org")
-let client = mqtt.connect("wss://mqtt.flespi.io", flespiConfig)
-
-const topics = ["tl4k/devices/+/data", "tl4k/devices/+/command"]
-
-client.on('connect', () => {
-    console.log("Connected to Flespi broker with client!")
-    // topics.forEach((topic) => {
-    //     console.log("Subscribing to topic: ", topic)
-    //     client.subscribe(topic)
-    // })
-
-    client.subscribe("tl4k/devices/#")
-})
-
-client.on('error', (error) => {
-    console.log("[Mqtt client] Error: " + JSON.stringify(error));
-})
-
-client['stream'].on('error', (error) => {
-    // fires when connection can't be established
-    // but not when an established connection is lost
-    console.log('[Mqtt client] stream error', error);
-    client.end();
 });
 
 client.on('message', (topic, message) => {
     console.log('Received message on Topic: ', topic);
 
+    if (!message){
+        console.log('Received empty message');
+        return;
+    }
+
+    let messageObject;
+
+    try{
+        messageObject = JSON.parse(message.toString());
+    }catch(exception){
+        console.log("Error parsing: " + message.toString());
+        return;
+    }
+
     if (topic.includes('data')) {
-        io.emit('mqtt data', JSON.parse(message.toString()));
+        io.emit('mqtt data', messageObject);
     } else if (topic.includes('command')) {
-        io.emit('mqtt command', JSON.parse(message.toString()));
+        io.emit('mqtt command', messageObject);
     } else {
-        io.emit('mqtt message', JSON.parse(message.toString()));
+        io.emit('mqtt message', messageObject);
     }
 
 });
